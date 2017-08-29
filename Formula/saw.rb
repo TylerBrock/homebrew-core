d29b8b445fb139bf17328a39ffaadc8a2cb46534
Tyler Brock <tyler.brock@gmail.com>
2017-08-29 06:55:18 -0700

    Add formula for saw

---
+++ Formula/saw.rb
@@ -0,0 +1,24 @@
+class Saw < Formula
+  desc ""
+  homepage "https://github.com/TylerBrock/saw"
+  url "https://github.com/TylerBrock/saw.git"
+  version "0.0.2"
+  head "https://github.com/TylerBrock/saw.git" 
+
+  depends_on "go" => :build
+  depends_on "dep" => :build
+
+  def install
+    ENV["GOPATH"] = buildpath
+    (buildpath/"src/github.com/TylerBrock/saw").install buildpath.children
+    cd "src/github.com/TylerBrock/saw" do
+      system "dep", "ensure"
+      system "go", "build"
+      bin.install "saw"
+    end
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/saw --version")
+  end
+end
